12f9caf16a524dfb954f18c4bda7ba9f483434c7
Andrew Hinh <ajhinh@gmail.com>
2023-02-24 09:09:06 -0800

    Update serve_ngrok.sh

--- frontend/serve_ngrok.sh
+++ frontend/serve_ngrok.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-while ! docker run -it -e NGROK_AUTHTOKEN="${NGROK_AUTHTOKEN}" ngrok/ngrok http 11700 --subdomain=captafied; do    
+while ! docker run -it -e NGROK_AUTHTOKEN="${NGROK_AUTHTOKEN}" ngrok/ngrok http 11701 --subdomain=captafied; do    
     sleep 1
 done
