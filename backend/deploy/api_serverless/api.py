bc84d385b5550c6f86fc5d2d345c816e53db7c57
Andrew Hinh <ajhinh@gmail.com>
2023-04-09 09:46:01 -0700

    Update api.py

--- backend/deploy/api_serverless/api.py
+++ backend/deploy/api_serverless/api.py
@@ -3,65 +3,34 @@ import json
 
 from PIL import ImageStat
 
-from question_answer.inference import Pipeline
-import question_answer.util as util
+from ..inference.inference import Pipeline
 
 model = Pipeline()
 
 
 def handler(event, _context):
     """Provide main prediction API."""
-    print("INFO loading image")
+    print("INFO loading input")
+    table = _load_table(event)
+    if table is None:
+        return {"statusCode": 400, "message": "table not found in event"}
+    requests = _load_requests(event)
+    if requests is None:
+        return {"statusCode": 400, "message": "requests not found in event"}
+    prev_answers = _load_prev_answers(event)
+    request_types = _load_request_types(event)
     image = _load_image(event)
-    if image is None:
-        return {"statusCode": 400, "message": "neither image_url nor image found in event"}
-    question = _load_question(event)
-    if question is None:
-        return {"statusCode": 400, "message": "neither question_url nor question found in event"}
-    print("INFO image loaded")
+    print("INFO input loaded")
     print("INFO starting inference")
-    pred = model.predict(image, question)
+    pred = model.predict(table, 
+                         requests,
+                         prev_answers,
+                         request_types,
+                         image)
     print("INFO inference complete")
-    image_stat = ImageStat.Stat(image)
-    print("METRIC image_mean_intensity {}".format(image_stat.mean[0]))
-    print("METRIC image_area {}".format(image.size[0] * image.size[1]))
-    print("METRIC pred_length {}".format(len(pred)))
+    print("METRIC num_pred_answers {}".format(sum(output is not None for output in pred)))
     print("INFO pred {}".format(pred))
-    return {"pred": str(pred)}
-
-
-def _load_image(event):
-    event = _from_string(event)
-    event = _from_string(event.get("body", event))
-    image_url = event.get("image_url")
-    if image_url is not None:
-        print("INFO url {}".format(image_url))
-        return util.read_image_pil(image_url)
-    else:
-        image = event.get("image")
-        if image is not None:
-            print("INFO reading image from event")
-            return util.read_b64_image(image)
-        else:
-            return None
-
-
-def _load_question(event):
-    event = _from_string(event)
-    event = _from_string(event.get("body", event))
-    question_url = event.get("question_url")
-    if question_url is not None:
-        print("INFO url {}".format(question_url))
-        with open(question_url, "r") as f:
-            question = f.readline()
-        return question
-    else:
-        question = event.get("question")
-        if question is not None:
-            print("INFO reading question from event")
-            return question
-        else:
-            return None
+    return {"pred": pred}
 
 
 def _from_string(event):
@@ -69,3 +38,58 @@ def _from_string(event):
         return json.loads(event)
     else:
         return event
+    
+    
+def get_event(event):
+    event = _from_string(event)
+    return _from_string(event.get("body", event))
+
+
+def _load_table(event):
+    event = get_event(event)
+    table = event.get("table")
+    if table is not None:
+        print("INFO reading table from event"))
+        return table
+    else:
+        return None
+        
+    
+def _load_requests(event):
+    event = get_event(event)
+    requests = event.get("requests")
+    if requests is not None:
+        print("INFO reading requests from event"))
+        return requests
+    else:
+        return None
+    
+
+def _load_prev_answers(event):
+    event = get_event(event)
+    prev_answers = event.get("prev_answers")
+    if prev_answers is not None:
+        print("INFO reading prev_answers from event"))
+        return prev_answers
+    else:
+        return None
+    
+    
+def _load_request_types(event):
+    event = get_event(event)
+    request_types = event.get("request_types")
+    if request_types is not None:
+        print("INFO reading request_types from event"))
+        return request_types
+    else:
+        return None
+    
+    
+def _load_image(event):
+    event = get_event(event)
+    image = event.get("image")
+    if image is not None:
+        print("INFO reading image from event")
+        return image
+    else:
+        return None
